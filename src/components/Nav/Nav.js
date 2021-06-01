26d31835d63c39a08813209f4660e3e94d71af72
wahyualfarisi <wahyualfarisi30@gmail.com>
2021-06-01 18:22:53 +0700

    finish nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './Nav.scss';
-import { FiCircle } from 'react-icons/fi';
 import { NavLink } from 'react-router-dom';
+import data from './NavData';
+import { FiArrowRightCircle } from 'react-icons/fi';
 
 function Nav() {
     return (
@@ -9,30 +10,22 @@ function Nav() {
             <div className="Navigation">
                 <h1 className="Navigation__heading mb-medium">Transaction List</h1>
                 <ul className="Navigation__items">
-                    <li className="Navigation__items_item">
-                        <NavLink to="/" className="Navigation__items_item_link">
-                            <FiCircle className="Navigation__items_item_icon"  />
-                            <p>INV-0001</p>
-                        </NavLink>
-                    </li>
-                    <li className="Navigation__items_item">
-                        <NavLink to="/" className="Navigation__items_item_link">
-                            <FiCircle className="Navigation__items_item_icon" />
-                            <p>INV-0002</p>
-                        </NavLink>
-                    </li>
-                    <li className="Navigation__items_item">
-                        <NavLink to="/" className="Navigation__items_item_link">
-                            <FiCircle className="Navigation__items_item_icon" />
-                            <p>INV-0003</p>
-                        </NavLink>
-                    </li>
-                    <li className="Navigation__items_item">
-                        <NavLink to="/" className="Navigation__items_item_link">
-                            <FiCircle className="Navigation__items_item_icon" />
-                            <p>INV-0004</p>
-                        </NavLink>
-                    </li>
+                    {data.map( (item, i) => {
+                        return (
+                            <li key={i} className="Navigation__items_item">
+                                <NavLink to="/" className="Navigation__items_item_link active">
+                                    <h3>{item.data}</h3>
+                                    <h4>{item.invoiceNumber}</h4>
+                                    <p>{item.total}</p>
+                                    <button>
+                                        Lihat Detail
+                                        <FiArrowRightCircle className="btn_icon" />
+                                    </button>
+                                </NavLink>
+                            </li>
+                        )
+                    })}
+                    
                 </ul>
             </div>
         </div>
---
+++ src/components/Nav/NavData.js
@@ -0,0 +1,29 @@
+const data = [
+    {
+        data: '21 Mei 2021',
+        total: 'Rp. 540.000',
+        invoiceNumber: 'INV-001'
+    },
+    {
+        data: '21 Mei 2021',
+        total: 'Rp. 540.000',
+        invoiceNumber: 'INV-002'
+    },
+    {
+        data: '21 Mei 2021',
+        total: 'Rp. 540.000',
+        invoiceNumber: 'INV-003'
+    },
+    {
+        data: '21 Mei 2021',
+        total: 'Rp. 1.540.000',
+        invoiceNumber: 'INV-004'
+    },
+    {
+        data: '21 Mei 2021',
+        total: 'Rp. 540.000',
+        invoiceNumber: 'INV-001'
+    }
+];
+
+export default data;
